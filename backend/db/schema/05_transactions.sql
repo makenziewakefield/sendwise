1040a568b4d0bb4d7a3ca4c30a4fa145cd6c0ead
Somto Ufondu <somtoufondu@gmail.com>
2024-09-25 15:55:20 -0600

    Update transactions table schema: rename amount to amount_in and amount_out

--- backend/db/schema/05_transactions.sql
+++ backend/db/schema/05_transactions.sql
@@ -1,11 +1,10 @@
--- Drop and recreate Widgets table (Example)
-
 DROP TABLE IF EXISTS transactions CASCADE;
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(user_id) ON DELETE SET NULL,
     category VARCHAR(50) NOT NULL,
-    amount DECIMAL(10, 2) NOT NULL,
+    amount_in DECIMAL(10, 2) NOT NULL,
+    amount_out DECIMAL(10, 2) NOT NULL,
     description TEXT,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
