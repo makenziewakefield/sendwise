50bfcb255e480e106f5e78babcbf5c56e85b1bbe
Grant Brown <47874434+Bigred0303@users.noreply.github.com>
2024-10-07 23:08:23 -0400

    Update 06_transactions.sql
    Revert to old transactions

--- backend/db/schema/06_transactions.sql
+++ backend/db/schema/06_transactions.sql
@@ -2,10 +2,9 @@ DROP TABLE IF EXISTS transactions CASCADE;
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(user_id) ON DELETE SET NULL,
-    category_id INT REFERENCES categories(id),
-    amount_in DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
-    amount_out DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    category VARCHAR(50) NOT NULL,
+    amount_in DECIMAL(10, 2) NOT NULL,
+    amount_out DECIMAL(10, 2) NOT NULL,
     description TEXT,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    transfer_id INTEGER REFERENCES transfers(id) ON DELETE CASCADE -- Link to Transfer
+    date DATE
 );
